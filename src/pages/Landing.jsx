eb914443426026d4a137635232d2a887d9b702bf
Alexis <alexiselie1912@gmail.com>
2026-05-26 18:28:44 +0200

    Fix crash: restore Link import removed by accident

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import Nav from '../components/Nav'
